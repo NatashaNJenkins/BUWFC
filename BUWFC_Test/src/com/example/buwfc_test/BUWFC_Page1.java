63694fe6ee67ac643a08d18b95a2fe7b5221c4e6
Qualix <natasha.micic@yahoo.co.uk>
2014-10-20 18:17:13 +0100

    Have added one button, a class ready to put the event entering activity in and Also started to create a DBAdapter

--- BUWFC_Test/src/com/example/buwfc_test/BUWFC_Page1.java
+++ BUWFC_Test/src/com/example/buwfc_test/BUWFC_Page1.java
@@ -2,14 +2,30 @@ package com.example.buwfc_test;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class BUWFC_Page1 extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_buwfc__page1);
+		
+		Button addEvent = (Button)findViewById(R.id.addEvent);
+		addEvent.setOnClickListener(new view.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(BUWFC_Page1.this, addEvent.class);
+				startActivity(i);
+				
+			}
+		})
+		
 	}
 
 	@Override
